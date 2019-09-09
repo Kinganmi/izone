26f5a9e865caecc9f6fc2e1843f11a0e08fc0641
huxihua <anmi_hu@163.com>
2019-09-09 21:20:04 +0800

    adjust

--- izone/settings.py
+++ izone/settings.py
@@ -210,10 +210,10 @@ REST_FRAMEWORK = {
 }
 
 # 配置数据库
-MYSQL_HOST = os.getenv('IZONE_MYSQL_HOST', '127.0.0.1')
-MYSQL_NAME = os.getenv('IZONE_MYSQL_NAME', 'izone')
+MYSQL_HOST = os.getenv('IZONE_MYSQL_HOST', '192.168.123.7')
+MYSQL_NAME = os.getenv('IZONE_MYSQL_NAME', 'my_izone')
 MYSQL_USER = os.getenv('IZONE_MYSQL_USER', 'root')
-MYSQL_PASSWORD = os.getenv('IZONE_MYSQL_PASSWORD', 'python')
+MYSQL_PASSWORD = os.getenv('IZONE_MYSQL_PASSWORD', 'Abcd@1234')
 MYSQL_PORT = os.getenv('IZONE_MYSQL_PORT', 3306)
 
 DATABASES = {
@@ -229,7 +229,7 @@ DATABASES = {
 }
 
 # 使用django-redis缓存页面，缓存配置如下：
-REDIS_HOST = os.getenv('IZONE_REDIS_HOST', '127.0.0.1')
+REDIS_HOST = os.getenv('IZONE_REDIS_HOST', '192.168.123.7')
 REDIS_PORT = os.getenv('IZONE_REDIS_PORT', 6379)
 
 CACHES = {
@@ -259,18 +259,18 @@ EMAIL_TIMEOUT = 5
 # 是否使用了SSL 或者TLS，为了用465端口，要使用这个
 EMAIL_USE_SSL = os.getenv('IZONE_EMAIL_USE_SSL', 'True').upper() == 'TRUE'
 # 默认发件人，不设置的话django默认使用的webmaster@localhost，所以要设置成自己可用的邮箱
-DEFAULT_FROM_EMAIL = os.getenv('IZONE_DEFAULT_FROM_EMAIL', 'TendCode博客 <your-email-address>')
+DEFAULT_FROM_EMAIL = os.getenv('IZONE_DEFAULT_FROM_EMAIL', 'L6BJ <your-email-address>')
 
 # 网站默认设置和上下文信息
-SITE_END_TITLE = os.getenv('IZONE_SITE_END_TITLE', 'izone')
-SITE_DESCRIPTION = os.getenv('IZONE_SITE_DESCRIPTION', 'izone 是一个使用 Django+Bootstrap4 搭建的个人博客类型网站')
-SITE_KEYWORDS = os.getenv('IZONE_SITE_KEYWORDS', 'izone,Django博客,个人博客')
+SITE_END_TITLE = os.getenv('IZONE_SITE_END_TITLE', '')
+SITE_DESCRIPTION = os.getenv('IZONE_SITE_DESCRIPTION', '是一个使用 Django+Bootstrap4 搭建的个人博客类型网站')
+SITE_KEYWORDS = os.getenv('IZONE_SITE_KEYWORDS', 'Django博客,个人博客')
 
 # 个性化设置，非必要信息
 # 个人 Github 地址
 MY_GITHUB = os.getenv('IZONE_GITHUB', 'https://github.com/Hopetree')
 # 工信部备案信息
-BEIAN = os.getenv('IZONE_BEIAN', '网站备案信息')
+BEIAN = os.getenv('IZONE_BEIAN', '苏ICP备19010454号')
 # 站长统计（友盟）
 CNZZ_PROTOCOL = os.getenv('IZONE_CNZZ_PROTOCOL', '')
 # 站长推送
